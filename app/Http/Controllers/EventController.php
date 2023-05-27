e7ef21b5ef51ddc6cbf07a165ddd5bf13c44a18b
Misla Wislaine <mislawislaine12@gmail.com>
2023-05-27 17:23:58 -0300

    Relação One to Many
    php artisan make:migration add_user_id_to_events_table
    
    php artisan migrate:fresh

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -53,6 +53,12 @@ class EventController extends Controller
 
             $event->image = $imageName;
         }
+
+        $user = auth()->user();
+
+        $event->user_id = $user->id;
+
+        
         
         $event->save();
 
