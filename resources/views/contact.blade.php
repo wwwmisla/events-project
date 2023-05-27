68f0d91cd03e393182f54d74ff36fdc73418e03e
Misla Wislaine <mislawislaine12@gmail.com>
2023-05-27 16:57:16 -0300

    Autenticação no Laravel
    composer require laravel/jetstream
    
    php artisan jetstream:install livewire
    
    php artisan migrate:status
    
    php artisan migrate:fresh
    
    php artisan migrate:status
    
    npm install
    
    npm run dev

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+
+@section('title', 'Contato')
+
+@section('content')
+    <h1>Está é a página contact</h1>
+    <a href="/">Voltar para home</a>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\EventController;
+
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/{id}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store']);
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
